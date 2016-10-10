2ced7a5af19a8e822db8e627fc511c8f87ed12b3
Paul Drummond <paul.drummond@iode.co.uk>
2016-10-10 11:20:38 +0100

    Initial work on moving dragging cards to columns

--- src/components/column/Column.js
+++ src/components/column/Column.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import { DropTarget } from 'react-dnd';
+import ItemTypes from '../../data/ItemTypes';
 import Card from '../card/Card';
 import './Column.css';
 
+const columnTarget = {
+  drop() {
+  }
+};
+
 class Column extends Component {
   render() {
-
     const {
+      canDrop, isOver, connectDropTarget,
       columnIndex,
       column: {
         title
       }
     } = this.props;
+    const isActive = /*canDrop &&*/ isOver;
+    console.log("BOOM:", this.props);
 
     return (
-      <div className="Column">
+      <div className="Column" style={{backgroundColor:(isActive?'red':'whitesmoke')}}>
         <div className="title">{title}</div>
         <div className="cards">
           {this.props.column.cards.map( (card, index) => {
@@ -25,4 +34,13 @@ class Column extends Component {
   }
 }
 
-export default Column;
+function collectDropTarget(connect, monitor) {
+  //console.log("BOOMX: ", monitor.getItem());
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  };
+}
+
+export default DropTarget(ItemTypes.CARD, columnTarget, collectDropTarget)(Column);
